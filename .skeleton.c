c9559436fc3536b47c928100e499512962ecae58
Matt Fichman <matt.fichman@gmail.com>
2015-12-25 17:05:26 -0500

    Added socket preliminary support

---
+++ .skeleton.c
@@ -0,0 +1,12 @@
+/* ========================================================================== --
+--                                                                            --
+-- Copyright (c) %year Matt Fichman <matt.fichman@gmail.com>                   --
+--                                                                            --
+-- This file is part of Quadrant.  It is subject to the license terms in the  --
+-- LICENSE.md file found in the top-level directory of this software package  --
+-- and at http://github.com/mfichman/quadrant/blob/master/LICENSE.md.         --
+-- No person may use, copy, modify, publish, distribute, sublicense and/or    --
+-- sell any part of Quadrant except according to the terms contained in the   --
+-- LICENSE.md file.                                                           --
+--                                                                            --
+-- ========================================================================== */

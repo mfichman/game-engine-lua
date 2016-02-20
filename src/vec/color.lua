68ce22663708b8ee4e3b2e6c2f24c2405c7e35ef
Matt Fichman <matt.fichman@gmail.com>
2016-02-20 11:01:37 -0500

    Add HSL constructor for Color

--- src/vec/color.lua
+++ src/vec/color.lua
@@ -55,5 +55,42 @@ function Color:data()
   return ffi.cast('vec_Scalar*', self)
 end
 
+-- Convert a hue, saturation, lightness tuple (where each component is in the
+-- range [0,1) to red-green-blue.
+function Color.hsl(hue, saturation, lightness)
+    if hue < 0 or hue > 1 then
+        return Color.new(0, 0, 0, 1)
+    end
+    if saturation < 0 or saturation > 1 then
+        return Color.new(0, 0, 0, 1)
+    end
+    if lightness < 0 or lightness > 1 then
+        return 0, 0, 0, alpha
+    end
+    local chroma = (1 - math.abs(2 * lightness - 1)) * saturation
+    local h = hue*360/60 --360/60
+    local x =(1 - math.abs(h % 2 - 1)) * chroma
+    local r, g, b = 0, 0, 0
+    if h < 1 then
+        r,g,b=chroma,x,0
+    elseif h < 2 then
+        r,b,g=x,chroma,0
+    elseif h < 3 then
+        r,g,b=0,chroma,x
+    elseif h < 4 then
+        r,g,b=0,x,chroma
+    elseif h < 5 then
+        r,g,b=x,0,chroma
+    else
+        r,g,b=chroma,0,x
+    end
+    local m = lightness - chroma/2
+    return Color.new(r+m,g+m,b+m,1)
+end
+
+setmetatable(Color, {
+  __call = function(t, ...) return Color.new(...) end
+})
+
 ffi.metatype(ColorType, Color)
-return Color.new
+return Color
